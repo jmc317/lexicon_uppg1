30a10784068a76a4e7ede30d57855e4b02ecdcd7
e16johka <e16johka@student.his.se>
2023-09-12 16:33:43 +0200

    Uppdatering

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
 ﻿string name;
 int salary;
+int selection;
 
-Console.WriteLine("Register a person with their name and salary: ");
-Console.WriteLine("Name: ");
-name = Console.ReadLine();
-Console.WriteLine("Salary: ");
-salary = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Registered " + name + " with monthly income: " + salary);
 
-/*Jag försökte göra en do-while loop så att man kan fortsätta registrera personal. I condition så ville jag ha att om man väljer 1. så lägger man till fler 
-och 2. så avslutas loopen. Problemet var att jag inte fick till konvertering från användares input för val (readline) som vad jag förstår i C# måste vara en string (?)
-till en int. Därför blev det ett väldigt basic program. */
+    Console.WriteLine("1. Register a person with their name and salary: ");
+    Console.WriteLine("2. Exit application ");
+    selection = Convert.ToInt32(Console.ReadLine());
+
+switch (selection)
+{
+    case 1:
+        Console.WriteLine("Name: ");
+        name = Console.ReadLine();
+        Console.WriteLine("Salary: ");
+        salary = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Registered " + name + " with monthly income: " + salary);
+        break;
+
+    case 2:
+        break;
+
+}
